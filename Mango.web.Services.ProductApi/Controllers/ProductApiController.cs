eebf135267c9696152990cea8180ec28c651df67
vasea <vasile.costiuc@it-lab.md>
2022-10-26 18:34:42 +0300

    ProductsApiController added

---
+++ Mango.web.Services.ProductApi/Controllers/ProductApiController.cs
@@ -0,0 +1,38 @@
+﻿using Mango.Services.ProductApi.Models.Dto;
+using Mango.Services.ProductApi.Repository;
+using Mango.web.Services.ProductApi;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ProductApi.Controllers
+{
+    public class ProductApiController : Controller
+    {
+        private IProductRepository _productRepository;
+
+        protected ResponseDto _response;
+
+        public ProductApiController(IProductRepository repository)
+        {
+            _productRepository = repository;
+            _response = new ResponseDto();
+        }
+
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            try
+            {
+                IEnumerable<ProductDto> products = await _productRepository.GetProducts();
+                _response.Result = products;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Errors = new List<string> { ex.ToString() };
+            }
+
+            return _response;
+        }
+
+    }
+}
